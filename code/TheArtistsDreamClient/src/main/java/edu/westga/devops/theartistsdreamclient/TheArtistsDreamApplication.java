9e3699810246129a9b88ff3daf626f18270abbdf
Aznella Joseph <ajoseph7@my.westga.edu>
2021-08-30 21:36:49 -0400

    Added FXML files
    Added FXML files for the main page of the application, where all of the artwork will be displayed. Created a control for each piece of artwork being displayed.

--- code/TheArtistsDreamClient/src/main/java/edu/westga/devops/theartistsdreamclient/TheArtistsDreamApplication.java
+++ code/TheArtistsDreamClient/src/main/java/edu/westga/devops/theartistsdreamclient/TheArtistsDreamApplication.java
@@ -11,30 +11,31 @@ import javafx.stage.Stage;
  */
 public class TheArtistsDreamApplication extends Application {
 
-    public static final String ICON_PATH = "icon.png";
+	public static final String ICON_PATH = "icon.png";
 
-    public static final String LOGIN_FXML = "view/TODO.fxml";
+	public static final String LOGIN_FXML = "view/TODO.fxml";
 
-    //TODO: SINGLETONS
+	// TODO: SINGLETONS
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(TheArtistsDreamApplication.class.getResource(LOGIN_FXML));
-        loader.load();
-        Scene scene = new Scene(loader.getRoot());
-        primaryStage.setTitle("The Artist's Dream");
-        primaryStage.setScene(scene);
-        primaryStage.getIcons().add(new Image(TheArtistsDreamApplication.class.getResourceAsStream(ICON_PATH)));
-        primaryStage.show();
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(TheArtistsDreamApplication.class.getResource(LOGIN_FXML));
+		loader.load();
+		Scene scene = new Scene(loader.getRoot());
+		primaryStage.setTitle("The Artist's Dream");
+		primaryStage.setScene(scene);
+		primaryStage.getIcons().add(new Image(TheArtistsDreamApplication.class.getResourceAsStream(ICON_PATH)));
+		primaryStage.setMaximized(true);
+		primaryStage.show();
+	}
 
-    /**
-     * Entry Point to running The Artist's Dream Application
-     *
-     * @param args the args for running the application
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+	/**
+	 * Entry Point to running The Artist's Dream Application
+	 *
+	 * @param args the args for running the application
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
